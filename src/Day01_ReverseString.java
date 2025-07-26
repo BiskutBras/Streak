73f535c30ac75792286c3da8374410a3654f532f
BiskutBras <s72430@ocean.umt.edu.my>
2025-07-26 09:27:18 +0800

    Add initial project structure with .gitignore and example classes Write Day 1 and Day 2 code.

---
+++ src/Day01_ReverseString.java
@@ -0,0 +1,23 @@
+public class Day01_ReverseString {
+
+
+    public static String reverseString(String str) {
+
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+
+            sb.append(str.charAt(i));
+
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String reversedString = reverseString("54321");
+        System.out.println(reversedString);
+
+    }
+}
---
+++ src/Day02_Person.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Day02_Person {
+
+
+    public static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Person p1 = new Person("abu", 39);
+        String p1Str = p1.toString();
+        System.out.println(p1Str);
+
+    }
+}
