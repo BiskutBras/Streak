8e3094f9bdeba0baf624cfb4d27e6dbc893f16b2
BiskutBras <syahkill375@gmail.com>
2025-07-29 11:02:31 +0800

    Add Day05_PalindromeChecker.java for palindrome checking functionality

---
+++ src/Day05_PalindromeChecker.java
@@ -0,0 +1,31 @@
+public class Day05_PalindromeChecker {
+
+    public static boolean palindromeCheck(String s) {
+
+        if (s == null) return false;
+
+        // clean the text and change case to lower case
+        String cleaned = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+
+        int leftPointer = 0;
+        int rightPointer = cleaned.length() - 1;
+
+        while (leftPointer < rightPointer) {
+            if (cleaned.charAt(leftPointer) != cleaned.charAt(rightPointer)) {
+                return false;
+            }
+            leftPointer++;
+            rightPointer--;
+
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        String word = "A man, a plan, a canal, Panama";
+        boolean isPalindrome = palindromeCheck(word);
+        System.out.println("Is palindrome: " + isPalindrome);
+
+    }
+}
