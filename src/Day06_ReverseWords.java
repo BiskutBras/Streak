c1c3ad56d914b2b39d8e3f20f7067518b341838b
BiskutBras <syahkill375@gmail.com>
2025-07-30 11:10:39 +0800

    Add new class that reverse words in sentence
    Manage to:
    split single word from the sentence
    
    Failed to:
    reverse each character
    split all word from sentence

---
+++ src/Day06_ReverseWords.java
@@ -0,0 +1,29 @@
+public class Day06_ReverseWords {
+
+    public static String reverseWords(String sentence) {
+
+        int leftPointer = 0;
+        int rightPointer = 0;
+
+        while (sentence.charAt(rightPointer) != ' ') {
+            rightPointer++;
+        }
+
+        String word = sentence.substring(leftPointer, rightPointer);
+
+
+        for (int i = rightPointer - 1; i >= leftPointer; i--) {
+            System.out.println(sentence.charAt(i));
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        String sentence = "Java is awesome";
+
+        String reverseString = reverseWords(sentence);
+
+        System.out.println(reverseString);
+    }
+}
