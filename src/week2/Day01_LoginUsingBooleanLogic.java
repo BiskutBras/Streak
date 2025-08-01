b7a08e7013a4025a80f6470267e7e03695ef4450
BiskutBras <syahkill375@gmail.com>
2025-08-01 08:14:24 +0800

    feat: add week2/Day1.java that check user credentials using boolean logic for login

---
+++ src/week2/Day01_LoginUsingBooleanLogic.java
@@ -0,0 +1,54 @@
+package week2;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Day01_LoginUsingBooleanLogic {
+
+    public static void userLogin() {
+
+        // valid input
+        Set<String> validUserList = new HashSet<>();
+        validUserList.add("BiskutBras");
+        validUserList.add("Syahrul");
+        validUserList.add("Username");
+        String validPassword = "bukalahi01";
+
+        // boolean logic
+        boolean isUsernameValid = false;
+        boolean isPasswordValid = false;
+
+        // user input
+        Scanner myScan = new Scanner(System.in);
+
+        System.out.println("Enter your username here");
+        String inputUsername = myScan.nextLine();
+
+        System.out.println("Enter the password");
+        String inputPassword = myScan.nextLine();
+
+        myScan.close();
+
+        // validate input
+        if (validUserList.contains(inputUsername)) {
+            isUsernameValid = true;
+        }
+        if (validPassword.equals(inputPassword)) {
+            isPasswordValid = true;
+        }
+
+        // print message
+        if (isUsernameValid && isPasswordValid) {
+            System.out.printf("Welcome %s", inputUsername);
+        } else {
+            System.out.println("Invalid credentials, please try again");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        userLogin();
+    }
+
+}
