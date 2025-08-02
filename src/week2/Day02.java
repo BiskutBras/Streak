13e0d5d4f4e854c7bdeae7498e1bff273bb630f9
BiskutBras <syahkill375@gmail.com>
2025-08-02 09:15:41 +0800

    feat: add week2/Day2.java that check if string has all unique characters

---
+++ src/week2/Day02.java
@@ -0,0 +1,27 @@
+package week2;
+
+import java.util.HashSet;
+
+public class Day02 {
+
+    public static boolean hasAllUniqueChars(String input) {
+        HashSet<Character> strSet = new HashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+
+            // return false if element already exist in set
+            if (!strSet.add(input.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(hasAllUniqueChars("abcdef"));
+        System.out.println(hasAllUniqueChars("hello"));
+        System.out.println(hasAllUniqueChars("Java123"));
+        System.out.println(hasAllUniqueChars("aA"));
+    }
+
+}
