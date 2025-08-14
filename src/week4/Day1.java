8d2be9cd5424b42b3702217de46d530039a268cb
BiskutBras <syahkill375@gmail.com>
2025-08-14 09:58:37 +0800

    feat: add week4/Day1.java that take sentence and return word from specific position

---
+++ src/week4/Day1.java
@@ -0,0 +1,22 @@
+package week4;
+
+public class Day1 {
+
+    public static String findTheWord(String sentence, int wordPosition) {
+
+        if (sentence == null || sentence.isBlank()) return "sentence is empty";
+        if (wordPosition <= 0) return "please insert valid position";
+
+        String[] words = sentence.split("\\s+");
+        if (wordPosition > words.length) return "exceed word count!";
+
+        return words[wordPosition - 1];
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(findTheWord("I love to eat", 2));
+
+    }
+}
